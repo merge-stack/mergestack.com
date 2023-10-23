47997b78041d6278bbf0662e618752899aa26e9b
UzeeChamp1 <usama.zahid@mergestack.com>
2023-10-23 12:08:12 +0500

    finalized styling

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -7,59 +7,75 @@ import {
   Box,
   Link,
   Container,
+  createTheme,
+  ThemeProvider,
 } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import Logo from 'public/assets/svg/Logo';
 
+const theme = createTheme({
+  components: {
+    MuiAppBar: {
+      styleOverrides: {
+        root: {
+          backgroundColor: 'white !important',
+          position: 'relative',
+        },
+      },
+    },
+    MuiLink: {
+      styleOverrides: {
+        root: {
+          color: '#000',
+          fontWeight: 500,
+          textDecoration: 'none',
+        },
+      },
+    },
+  },
+});
+
 const Header = () => {
   return (
-    <AppBar sx={{ backgroundColor: 'white', position: 'relative' }}>
-      <Container maxWidth="lg">
-        <Grid
-          container
-          maxWidth="lg"
-          justifyContent="space-between"
-          alignItems="center"
-        >
-          <Box display="flex" alignItems="center" gap="1rem">
-            <Logo />
-          </Box>
-
-          <Box display="flex" alignItems="center" gap="2rem">
+    <ThemeProvider theme={theme}>
+      <AppBar>
+        <Container maxWidth="lg">
+          <Grid
+            container
+            maxWidth="lg"
+            justifyContent="space-between"
+            alignItems="center"
+          >
             <Box display="flex" alignItems="center" gap="1rem">
-              <Link underline="none" fontWeight="500" color="#000">
-                About
-              </Link>
-              <Link underline="none" fontWeight="500" color="#000">
-                Services
-              </Link>
-              <Link underline="none" fontWeight="500" color="#000">
-                Portfolio
-              </Link>
-              <Link underline="none" fontWeight="500" color="#000">
-                Careers
-              </Link>
-              <Link underline="none" fontWeight="500" color="#000">
-                Blogs
-              </Link>
+              <Logo />
             </Box>
-            <Box display="flex" alignItems="center" gap="1rem">
-              <Button
-                sx={{ backgroundColor: '#4FB0E3', borderRadius: '25px' }}
-                color="secondary"
-                variant="contained"
-              >
-                Contact Us
-              </Button>
-              {/* 
+
+            <Box display="flex" alignItems="center" gap="2rem">
+              <Box display="flex" alignItems="center" gap="1rem">
+                <Link href='#'>About</Link>
+                <Link href='#'>Services</Link>
+                <Link href='#'>Portfolio</Link>
+                <Link href='#'>Careers</Link>
+                <Link href='#'>Blogs</Link>
+              </Box>
+              <Box display="flex" alignItems="center" gap="1rem">
+                <Button
+                  sx={{ backgroundColor: '#4FB0E3', borderRadius: '25px' }}
+                  color="secondary"
+                  variant="contained"
+                >
+                  Contact Us
+                </Button>
+                {/* 
               <IconButton color="#000" edge="end">
                 <MenuIcon />
               </IconButton> */}
+              </Box>
             </Box>
-          </Box>
-        </Grid>
-      </Container>
-    </AppBar>
+          </Grid>
+        </Container>
+      </AppBar>
+    </ThemeProvider>
   );
 };
 
