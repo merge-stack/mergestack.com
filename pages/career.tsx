03bdcf76db38009a19901c89b6eef0ffec5398f8
UzeeChamp1 <usama.zahid@mergestack.com>
2023-11-03 12:47:44 +0500

    Career Text Section Designed

--- pages/career.tsx
+++ pages/career.tsx
@@ -3,11 +3,13 @@ import { Box } from '@mui/material';
 
 import LayoutHoc from 'src/layoutHoc';
 import { CareersBanner } from 'src/components/career/banner';
+import { CareerTextSection } from 'src/components/career/career';
 
 const Career: React.FC = (): React.ReactNode => {
   return (
     <Box component="div">
       <CareersBanner/>
+      <CareerTextSection/>
     </Box>
   );
 };
