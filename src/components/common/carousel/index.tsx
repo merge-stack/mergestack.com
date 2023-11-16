0d69fefc92d8ce44483de4d74161fe4266325bd4
UzeeChamp1 <usama.zahid@mergestack.com>
2023-11-16 13:16:19 +0500

    Animation Done

--- src/components/common/carousel/index.tsx
+++ src/components/common/carousel/index.tsx
@@ -1,7 +1,5 @@
-// components/ImageCarousel.tsx
-
 import React, { useState, useEffect } from 'react';
-import { Container, Box, Typography, Theme } from '@mui/material';
+import { Container, Box, Theme } from '@mui/material';
 import { makeStyles } from '@mui/styles';
 
 interface Image {
@@ -13,33 +11,49 @@ interface ICarouselProps {
   images: Image[];
 }
 
-const useStyles = makeStyles((theme: Theme) => ({
+const useStyles = makeStyles<Theme>((theme: Theme) => ({
   slideImgBox: {
     display: 'flex',
     justifyContent: 'space-between',
     maxHeight: '100%',
+    overflow: 'hidden',
     '@media (max-width: 1000px)': {
       flexWrap: 'wrap',
       gap: '2rem',
-      alignItems: 'center',
+      justifyContent: 'center',
       '& > img': {
         width: 'fit-content',
       },
     },
+  },
+  slideAnimation: {
+    display: 'flex',
+    transition: 'transform 1s ease-in-out',
+  },
+  slideOut: {
+    transform: 'translateX(-100vw)',
+  },
+  slideIn: {
+    transform: 'translateX(0%)',
   },
 }));
 
 const ImageCarousel: React.FC<ICarouselProps> = ({ images }) => {
   const classes = useStyles();
   const [currentSetIndex, setCurrentSetIndex] = useState(0);
+  const [slideOut, setSlideOut] = useState(false);
 
   const imagesPerSet = 4;
   const totalSets = Math.ceil(images.length / imagesPerSet);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCurrentSetIndex((prevIndex) => (prevIndex + 1) % totalSets);
-    }, 3000);
+      setSlideOut(true);
+      setTimeout(() => {
+        setCurrentSetIndex((prevIndex) => (prevIndex + 1) % totalSets);
+        setSlideOut(false);
+      }, 1000);
+    }, 6000);
 
     return () => clearInterval(interval);
   }, [totalSets]);
@@ -50,7 +64,15 @@ const ImageCarousel: React.FC<ICarouselProps> = ({ images }) => {
     const visibleImages = images.slice(start, end);
 
     return visibleImages.map((image, index) => (
-      <Box key={index} component="img" src={image.img} alt={image.alt} />
+      <Box
+        key={index}
+        component="img"
+        src={image.img}
+        alt={image.alt}
+        className={`${classes.slideAnimation} ${
+          slideOut ? classes.slideOut : classes.slideIn
+        }`}
+      />
     ));
   };
 
